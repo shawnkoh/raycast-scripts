4a9f6499d4c357755dba314e687ce2627b9f4b9c
Shawn Koh <shawn@shawnkoh.sg>
2021-10-23 23:54:04 +0800

    Replace copy current date

--- copy-current-date.py
+++ copy-current-date.py
@@ -2,21 +2,19 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Open daily note
-# @raycast.mode inline
+# @raycast.title Copy current date
+# @raycast.mode silent
 # @raycast.refreshTime 1h
 
 # Optional parameters:
 # @raycast.icon
-# @raycast.packageName sg.shawnkoh.open-daily-note
+# @raycast.packageName sg.shawnkoh.copy-current-date
 
 # Documentation:
 # @raycast.author Shawn Koh
 # @raycast.authorURL https://github.com/shawnkoh
 
-import webbrowser
 from datetime import *
 
 date = datetime.today().strftime("%Y-%m-%d")
-url = f"bear://x-callback-url/open-note?title={date}&new_window=yes&edit=yes"
-webbrowser.open(url)
+print(date)
