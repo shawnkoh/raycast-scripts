0d208b791e578e24ae7dc58a130f4e50eb96a919
Shawn Koh <shawn@shawnkoh.sg>
2021-10-26 04:51:58 +0800

    Update backlinks script

--- bear-backlinks.sh
+++ bear-backlinks.sh
@@ -3,7 +3,7 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Refresh bear backlinks
-# @raycast.mode silent
+# @raycast.mode fullOutput
 # @raycast.refreshTime 1h
 
 # Optional parameters:
@@ -14,12 +14,22 @@
 # @raycast.author Shawn Koh
 # @raycast.authorURL https://github.com/shawnkoh
 
-cd /Users/shawnkoh/repos/notes &&
+cd /Users/shawnkoh/repos/notes ;
 git add -A && git commit -m "Commit unexpected changes" ;
-python3 /Users/shawnkoh/repos/Bear-Markdown-Export/bear_export_sync.py --out /Users/shawnkoh/repos/notes/bear --backup /Users/shawnkoh/repos/notes/bear-backup && 
+
+python3 /Users/shawnkoh/repos/Bear-Markdown-Export/bear_export_sync.py --out /Users/shawnkoh/repos/notes/bear --backup /Users/shawnkoh/repos/notes/bear-backup ;
 git add -A && git commit -m "Commit exported changes from Bear" ;
-/usr/local/bin/node /Users/shawnkoh/.config/yarn/global/node_modules/@andymatuschak/note-link-janitor/dist/index.js /Users/shawnkoh/repos/notes/bear &&
+
+python3 /Users/shawnkoh/repos/raycast-scripts/cleanup-references.py ;
+git add -A && git commit -m "Cleanup references" ;
+
+python3 /Users/shawnkoh/repos/raycast-scripts/cleanup-tags.py ;
+git add -A && git commit -m "Cleanup tags" ;
+
+/usr/local/bin/node /Users/shawnkoh/.config/yarn/global/node_modules/@andymatuschak/note-link-janitor/dist/index.js /Users/shawnkoh/repos/notes/bear ;
 git add -A && git commit -m "Commit changes from note-link-janitor" ;
-python3 /Users/shawnkoh/repos/Bear-Markdown-Export/bear_export_sync.py --out /Users/shawnkoh/repos/notes/bear --backup /Users/shawnkoh/repos/notes/bear-backup &&
+
+python3 /Users/shawnkoh/repos/Bear-Markdown-Export/bear_export_sync.py --out /Users/shawnkoh/repos/notes/bear --backup /Users/shawnkoh/repos/notes/bear-backup ;
 git add -A && git commit -m "Commit imported changes to Bear" ;
+
 git push
