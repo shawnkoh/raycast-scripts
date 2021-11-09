ead501dca7c2e72521bbe9b09910a40646b5b34e
Shawn Koh <shawn@shawnkoh.sg>
2021-11-09 19:05:30 +0800

    Add create-vscode script

---
+++ script-commands/create-vscode.sh
@@ -0,0 +1,20 @@
+#!/opt/homebrew/bin/fish --login
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Create VSCode Project
+# @raycast.mode silent
+# @raycast.refreshTime 1h
+# @raycast.argument1 { "type": "text", "placeholder": "project name" }
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName sg.shawnkoh.vscode-create
+
+# Documentation:
+# @raycast.author Shawn Koh
+# @raycast.authorURL https://github.com/shawnkoh
+
+mkdir -p ~/repos/$argv[1]
+code ~/repos/$argv[1]
+git init
