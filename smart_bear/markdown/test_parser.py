027406215f899617580c4d88bbd2869129189aa7
Shawn Koh <shawn@shawnkoh.sg>
2022-02-25 18:50:12 +0800

    Add parser

--- smart_bear/markdown/test_parser.py
+++ smart_bear/markdown/test_parser.py
@@ -1,5 +1,15 @@
-import parsy
-from smart_bear.markdown.lexer import QuestionPrefix, AnswerPrefix, Text
+from pprint import pprint
+from typing import Optional
+from parsy import seq
+from smart_bear.markdown.lexer import (
+    QuestionPrefix,
+    AnswerPrefix,
+    separator,
+    Text,
+    question_prefix,
+    answer_prefix,
+    text,
+)
 
 from attrs import define
 
@@ -10,6 +20,61 @@ class Question:
     text: Text
 
 
+@define
 class Answer:
     prefix: AnswerPrefix
     text: Text
+
+
+@define
+class BasicPrompt:
+    question: Question
+    answer: Optional[Answer]
+
+
+question = seq(prefix=question_prefix, text=text).combine_dict(Question)
+answer = seq(prefix=answer_prefix, text=text).combine_dict(Answer)
+
+basic_prompt = (
+    seq(
+        question=question,
+        _separator=separator,
+        answer=answer.optional(),
+    ).combine_dict(BasicPrompt)
+    | question.map(lambda x: BasicPrompt(question=x, answer=None))
+)
+
+
+def test_question():
+    given = "Q: Question\nExtended"
+    expected = Question(prefix=QuestionPrefix("Q: "), text=Text("Question\nExtended"))
+    assert question.parse(given) == expected
+
+
+def test_answer():
+    given = "A: Answer\nExtended"
+    expected = Answer(prefix=AnswerPrefix("A: "), text=Text("Answer\nExtended"))
+    assert answer.parse(given) == expected
+
+
+def test_basic_prompt():
+    given = "Q: Question\nExtended\nA: Answer\nExtended"
+    expected = BasicPrompt(
+        question=Question(
+            prefix=QuestionPrefix("Q: "), text=Text("Question\nExtended")
+        ),
+        answer=Answer(prefix=AnswerPrefix("A: "), text=Text("Answer\nExtended")),
+    )
+    assert basic_prompt.parse(given) == expected
+
+
+def test_basic_prompt_question_only():
+    given = "Q: Question\nExtended"
+    expected = BasicPrompt(
+        question=Question(
+            prefix=QuestionPrefix("Q: "), text=Text("Question\nExtended")
+        ),
+        answer=None,
+    )
+    pprint(basic_prompt.parse(given))
+    assert basic_prompt.parse(given) == expected
