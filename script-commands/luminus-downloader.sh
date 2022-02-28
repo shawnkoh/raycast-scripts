53014847ca13a7ebd0c7e44e070f9a5fa729b235
Shawn Koh <shawn@shawnkoh.sg>
2022-02-28 20:39:30 +0800

    Add luminus script

---
+++ script-commands/luminus-downloader.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Luminus Downloader
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+cd ~/repos/luminus-downloader
+/Users/shawnkoh/.asdf/shims/node main.js
