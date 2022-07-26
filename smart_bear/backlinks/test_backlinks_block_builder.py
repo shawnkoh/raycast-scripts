435c23ac30d170d7ed01cc95c884604d600ad46e
Shawn Koh <shawn@shawnkoh.sg>
2022-07-26 18:49:38 +0800

    Add more test cases

--- smart_bear/backlinks/test_backlinks_block_builder.py
+++ smart_bear/backlinks/test_backlinks_block_builder.py
@@ -34,3 +34,109 @@ def test_build():
     )
 
     assert backlinks_block_builder.build(given) == expected
+
+
+def test_build_2():
+    given = [
+        Edge(
+            from_node=Title("Something that links here"),
+            to_node=Backlink("Sample note"),
+            children=[
+                InlineText(
+                    "The block of text in the referencing note which contains the link to "
+                ),
+                Backlink("Sample note"),
+                EOL(),
+            ],
+        ),
+        Edge(
+            from_node=Title("Something that links here"),
+            to_node=Backlink("Sample note"),
+            children=[
+                InlineText("Another block in that same note which links to "),
+                Backlink("Sample note"),
+                EOL(),
+            ],
+        ),
+    ]
+
+    expected = BacklinksBlock(
+        [
+            InlineText("* "),
+            Backlink("Something that links here"),
+            EOL(),
+            InlineText("\t* "),
+            InlineText(
+                "The block of text in the referencing note which contains the link to "
+            ),
+            Backlink("Sample note"),
+            EOL(),
+            InlineText("\t* "),
+            InlineText("Another block in that same note which links to "),
+            Backlink("Sample note"),
+            EOL(),
+        ]
+    )
+
+    assert backlinks_block_builder.build(given) == expected
+
+
+def test_build_3():
+    given = [
+        Edge(
+            from_node=Title("Something that links here"),
+            to_node=Backlink("Sample note"),
+            children=[
+                InlineText(
+                    "The block of text in the referencing note which contains the link to "
+                ),
+                Backlink("Sample note"),
+                EOL(),
+            ],
+        ),
+        Edge(
+            from_node=Title("Something that links here"),
+            to_node=Backlink("Sample note"),
+            children=[
+                InlineText("Another block in that same note which links to "),
+                Backlink("Sample note"),
+                EOL(),
+            ],
+        ),
+        Edge(
+            from_node=Title("A different note that links here"),
+            to_node=Backlink("Sample note"),
+            children=[
+                InlineText("This is a paragraph from another note which links to "),
+                Backlink("Sample note"),
+                EOL(),
+            ],
+        ),
+    ]
+
+    expected = BacklinksBlock(
+        [
+            InlineText("* "),
+            Backlink("Something that links here"),
+            EOL(),
+            InlineText("\t* "),
+            InlineText(
+                "The block of text in the referencing note which contains the link to "
+            ),
+            Backlink("Sample note"),
+            EOL(),
+            InlineText("\t* "),
+            InlineText("Another block in that same note which links to "),
+            Backlink("Sample note"),
+            EOL(),
+            InlineText("* "),
+            Backlink("A different note that links here"),
+            EOL(),
+            InlineText("\t* "),
+            InlineText("This is a paragraph from another note which links to "),
+            Backlink("Sample note"),
+            EOL(),
+        ]
+    )
+
+    assert backlinks_block_builder.build(given) == expected
