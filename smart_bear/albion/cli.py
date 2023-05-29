95870598d88186e6b3fd908028116c3f0c7ef84b
Shawn Koh <shawn@shawnkoh.sg>
2023-05-29 15:59:48 +0800

    Fix

--- smart_bear/albion/cli.py
+++ smart_bear/albion/cli.py
@@ -4,6 +4,9 @@ import json
 from rich.pretty import pprint
 from rich import inspect
 import typer
+import aiohttp
+import aiorun
+import uvloop
 
 app = typer.Typer()
 
@@ -12,6 +15,47 @@ app = typer.Typer()
 # 1. get a list of every craftable item
 # 2. query the api to get their prices
 # 3. for every craftable item, check the profitabiliy of crafting within the city
+
+API_URL = "https://east.albion-online-data.com/api/v2/stats"
+MAX_URL_LENGTH = 4096
+
+
+class ApiClient:
+    client: aiohttp.ClientSession
+
+    def __init__(self, client: aiohttp.ClientSession) -> None:
+        self.client = client
+
+    async def get_prices(self, name_list: list[str], format: str = "json"):
+        assert len(name_list) > 0
+
+        result = list()
+
+        url = f"{API_URL}/Prices/"
+        format_suffix = f".{format}"
+        max_url_length = MAX_URL_LENGTH - len(url) - len(format_suffix)
+
+        url_chunks = list[str]()
+        url_chunk = ""
+
+        for index, name in enumerate(name_list):
+            new_url_chunk = f"{url_chunk}{',' if url_chunk == '' else ''}{name}"
+
+            if len(new_url_chunk) > max_url_length:
+                url_chunks.append(f"{url}{url_chunk}{format_suffix}")
+                url_chunk = name
+            else:
+                url_chunk = new_url_chunk
+
+            if len(name_list) == index + 1:
+                url_chunks.append(f"{url}{url_chunk}{format_suffix}")
+
+        for url_chunk in url_chunks:
+            async with self.client.get(url_chunk) as response:
+                result_chunk = await response.json()
+                result.append(result_chunk)
+
+        return result
 
 
 def is_craftable_item(item: dict) -> bool:
@@ -37,8 +81,9 @@ def parse_dict(craftable_items: list[dict], subject: dict):
                     parse_dict(craftable_items, item)
 
 
-@app.command()
-def init():
+async def main(loop: uvloop.Loop):
+    session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=None))
+    api_client = ApiClient(session)
     db = Database("albion.db")
     dotenv.load_dotenv()
     ITEMS_PATH = "/Users/shawnkoh/repos/ao-data/ao-bin-dumps/items.json"
@@ -47,7 +92,24 @@ def init():
 
     craftable_items = list[dict]()
     parse_dict(craftable_items, items_json["items"])
-    pprint(craftable_items)
+
+    craftable_items_uniquename = list()
+    for item in craftable_items:
+        craftable_items_uniquename.append(item["@uniquename"])
+
+    # pprint(await api_client.get_prices(craftable_items_uniquename))
+    await api_client.get_prices(craftable_items_uniquename)
+
+    await session.close()
+    loop.stop()
+
+
+@app.command()
+def init():
+    loop = uvloop.new_event_loop()
+    aiorun.run(main(loop), loop=loop)
+
+    # pprint(craftable_items)
     # pprint(items_json["items"])
 
 
