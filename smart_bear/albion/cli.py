467e54372cfde29ed4446696af386c6c21eb9b87
Shawn Koh <shawn@shawnkoh.sg>
2023-05-30 15:46:19 +0800

    Move to models

--- smart_bear/albion/cli.py
+++ smart_bear/albion/cli.py
@@ -1,9 +1,6 @@
-import pendulum
-from cattrs.gen import make_dict_structure_fn, override
 from typing import Any, Callable, Optional
 from sqlite_utils import Database
 from sqlite_utils.db import NotFoundError
-import dotenv
 import json
 import attrs
 import expression
@@ -14,115 +11,15 @@ import aiohttp
 import aiorun
 import uvloop
 from .api import ApiClient
-import cattrs
-from pendulum.datetime import DateTime
+from .models import (
+    ItemPrice,
+    CraftableItem,
+    CraftResource,
+    CraftingRequirement,
+    converter,
+)
 
 ITEMS_PATH = "/Users/shawnkoh/repos/ao-data/ao-bin-dumps/items.json"
-
-
-@attrs.frozen
-class ItemPrice:
-    id: str
-    city: str
-    quality: int
-    sell_price_min: float
-    sell_price_min_date: DateTime
-    sell_price_max: float
-    sell_price_max_date: DateTime
-    buy_price_min: float
-    buy_price_min_date: DateTime
-    buy_price_max: float
-    buy_price_max_date: DateTime
-
-
-@attrs.frozen
-class CraftResource:
-    id: str
-    count: int
-    max_return_amount: int | None = None
-
-
-@attrs.frozen
-class CraftingRequirement:
-    time: float | None = None
-    craft_resource: CraftResource | list[CraftResource] | None = None
-    silver: float | None = None
-    crafting_focus: float | None = None
-
-
-@attrs.frozen
-class CraftableItem:
-    id: str
-    crafting_requirements: CraftingRequirement | list[CraftingRequirement]
-
-
-converter = cattrs.Converter()
-
-converter.register_unstructure_hook(DateTime, lambda dt: dt.timestamp())
-
-converter.register_structure_hook(DateTime, lambda ts, _: pendulum.parser.parse(ts))
-
-
-converter.register_structure_hook(
-    ItemPrice,
-    make_dict_structure_fn(
-        ItemPrice,
-        converter,
-        id=override(rename="item_id"),
-    ),
-)
-
-converter.register_structure_hook(
-    CraftResource,
-    make_dict_structure_fn(
-        CraftResource,
-        converter,
-        id=override(rename="@uniquename"),
-        count=override(rename="@count"),
-        max_return_amount=override(rename="@maxreturnamount"),
-    ),
-)
-
-converter.register_structure_hook(
-    CraftResource | list[CraftResource] | None,
-    lambda craft_resource, _: [converter.structure(craft_resource, CraftResource)]
-    if isinstance(craft_resource, dict)
-    else converter.structure(craft_resource, list[CraftResource])
-    if isinstance(craft_resource, list)
-    else None,
-)
-
-
-converter.register_structure_hook(
-    CraftingRequirement,
-    make_dict_structure_fn(
-        CraftingRequirement,
-        converter,
-        time=override(rename="@time"),
-        silver=override(rename="@silver"),
-        craft_resource=override(rename="craftresource"),
-        crafting_focus=override(rename="@craftingfocus"),
-    ),
-)
-
-converter.register_structure_hook(
-    CraftingRequirement | list[CraftingRequirement],
-    lambda crafting_requirement, _: [
-        converter.structure(crafting_requirement, CraftingRequirement)
-    ]
-    if isinstance(crafting_requirement, dict)
-    else converter.structure(crafting_requirement, list[CraftingRequirement]),
-)
-
-converter.register_structure_hook(
-    CraftableItem,
-    make_dict_structure_fn(
-        CraftableItem,
-        converter,
-        id=override(rename="@uniquename"),
-        crafting_requirements=override(rename="craftingrequirements"),
-    ),
-)
 
 
 app = typer.Typer()
